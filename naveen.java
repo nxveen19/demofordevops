0b49825530d5908f2bcf9736a77fb90cc78a80ea
nxveen19 <125440125+nxveen19@users.noreply.github.com>
2023-11-03 07:10:02 +0530

    jenkins

---
+++ naveen.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of Fibonacci terms to generate: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.println("Fibonacci Series with " + n + " terms:");
+
+            int firstTerm = 0, secondTerm = 1;
+            for (int i = 0; i < n; i++) {
+                System.out.print(firstTerm + " ");
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+        }
+
+        scanner.close();
+    }
+}
